591a028db68484e5fc06cfaf9d5b9bd0559b53a7
MarioGK <mariogk01@gmail.com>
2023-05-23 18:53:03 -0300

    Initial C# Implementation

---
+++ csharp/lexer/Program.cs
@@ -0,0 +1,11 @@
+﻿using lexer;
+
+const string testInput = "=+(){},;";
+
+var tokens = Lexer.ParseTokens(testInput);
+
+foreach (var token in tokens)
+{
+    Console.WriteLine(token);
+}
+
---
+++ csharp/lexer/Tokens.cs
@@ -0,0 +1,19 @@
+﻿namespace lexer;
+
+public enum Tokens
+{
+    Ident,
+    Integer,
+    Let,
+    Illegal,
+    Eof,
+    Equal,
+    Plus,
+    Comma,
+    Semicolon,
+    LParen,
+    RParen,
+    LSquirly,
+    RSquirly,
+    Function
+}
