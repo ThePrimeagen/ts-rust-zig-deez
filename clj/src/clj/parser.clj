4ea4d8ff3213e2ec16545e9a88ed627d16f7dce6
Vikas <v33vikasgautam@gmail.com>
2023-05-29 05:17:51 +0530

    fix: used more domain specific variable names

--- clj/src/clj/parser.clj
+++ clj/src/clj/parser.clj
@@ -3,6 +3,9 @@
             [clj.ast   :as ast]
             [clj.lexer :as lexer]))
 
+(defn return [parsed rest-tokens]
+  [parsed rest-tokens])
+
 (declare parse-expr)
 
 ;; precedence
@@ -16,34 +19,34 @@
 
 (defn precedence [token-type]
   (case token-type
-    :eq      EQUALS
-    :noteq   EQUALS
-    :lt      LTGT
-    :gt      LTGT
-    :gteq    LTGT
-    :lteq    LTGT
-    :plus    SUM
-    :minus   SUM
-    :slash   PRODUCT
-    :astrisk PRODUCT
-             LOWEST))
+    (:eq
+     :noteq)   EQUALS
+    (:lt
+     :gt
+     :gteq
+     :lteq)    LTGT
+    (:plus
+     :minus)   SUM
+    (:slash
+     :astrisk) PRODUCT
+               LOWEST))
 
-(defn chomp-semicolon [lexed]
-  (drop-while #(= :semicolon (token/kind %)) lexed))
+(defn chomp-semicolon [tokens]
+  (drop-while #(= :semicolon (token/kind %)) tokens))
 
-(defn parse-ident [[ident & rst :as lexed]]
-  [(ast/ident (token/literal ident)) rst])
+(defn parse-ident [[ident :as tokens]]
+  (return (ast/ident (token/literal ident)) (rest tokens)))
 
-(defn parse-int [[int & rst :as lexed]]
-  [(ast/int (Integer/parseInt (token/literal int))) rst])
+(defn parse-int [[int :as tokens]]
+  (return (ast/int (Integer/parseInt (token/literal int))) (rest tokens)))
 
-(defn parse-prefix [[prefix & rst :as lexed]]
-  (when-let [[right-expr rst] (parse-expr PREFIX (next lexed))]
-    [(ast/prefix (token/literal prefix) right-expr) rst]))
+(defn parse-prefix [[prefix :as tokens]]
+  (when-let [[right-expr rest-tokens] (parse-expr PREFIX (rest tokens))]
+    (return (ast/prefix (token/literal prefix) right-expr) rest-tokens)))
 
-(defn parse-infix [left-expr [infix & rst :as lexed]]
-  (when-let [[right-expr rst] (parse-expr (precedence (token/kind infix)) rst)]
-    [(ast/infix left-expr (token/literal infix) right-expr) rst]))
+(defn parse-infix [left-expr [infix :as tokens]]
+  (when-let [[right-expr rest-tokens] (parse-expr (precedence (token/kind infix)) (rest tokens))]
+    (return (ast/infix left-expr (token/literal infix) right-expr) rest-tokens)))
 
 (defn prefix-parse-fn [token-type]
   (case token-type
@@ -68,37 +71,37 @@
                nil))
 
 (defn parse-expr 
-  ([prece [expr :as lexed]]
+  ([prece [expr :as tokens]]
     (when-let [prefix    (prefix-parse-fn (token/kind expr))]
-    (when-let [left-expr (prefix lexed)]
+    (when-let [left-expr (prefix tokens)] ;; left-expr = [left-expr rest-tokens]
       (apply parse-expr prece left-expr))))
-  ([prece left-expr [op :as rst]]
+  ([prece left-expr [op :as rest-tokens]]
     (if-not (and (not= :semicolon (token/kind op))
-                 (< prece (precedence (token/kind op))))  [left-expr rst]
+                 (< prece (precedence (token/kind op))))  (return left-expr rest-tokens)
     (if-let   [infix     (infix-parse-fn (token/kind op))]
-    (when-let [left-expr (infix left-expr rst)]
-      (apply parse-expr prece left-expr))                 [left-expr rst]))))
+    (when-let [left-expr (infix left-expr rest-tokens)]
+      (apply parse-expr prece left-expr))                 (return left-expr rest-tokens)))))
 
-(defn parse-let [[ident op & rst :as lexed]]
+(defn parse-let [[ident op & rest-tokens :as tokens]]
   (when (= :assign (token/kind op))
-  (when-let [[val rst] (parse-expr LOWEST rst)]
-    [(ast/let (token/literal ident) val) (chomp-semicolon rst)])))
+  (when-let [[val rest-tokens] (parse-expr LOWEST rest-tokens)]
+    (return (ast/let (token/literal ident) val) (chomp-semicolon rest-tokens)))))
 
-(defn parse-expr-stmt [lexed]
-  (when-let [[expr rst] (parse-expr LOWEST lexed)]
-    [(ast/expr expr) (chomp-semicolon rst)]))
+(defn parse-expr-stmt [tokens]
+  (when-let [[expr rest-tokens] (parse-expr LOWEST tokens)]
+    (return (ast/expr expr) (chomp-semicolon rest-tokens))))
 
-(defn parse-stmt [[cur & rst :as lexed]]
+(defn parse-stmt [[cur :as tokens]]
   (case (token/kind cur)
-    :let    (parse-let rst)
+    :let    (parse-let (rest tokens))
     :return nil
     :if     nil
-            (parse-expr-stmt lexed)))
+            (parse-expr-stmt tokens)))
 
-(defn parse-program [[cur :as lexed]]
+(defn parse-program [[cur :as tokens]]
   (when-not (= :eof (token/kind cur))
-  (when-let [[stmt rst] (parse-stmt lexed)]
-    (cons stmt (lazy-seq (parse-program rst))))))
+  (when-let [[stmt rest-tokens] (parse-stmt tokens)]
+    (cons stmt (lazy-seq (parse-program rest-tokens))))))
 
 (comment
   (parse-program (lexer/lex "foobar;"))
@@ -108,6 +111,7 @@
   (parse-program (lexer/lex "5 + 5;"))
   (parse-program (lexer/lex "5 != 5;"))
   (parse-expr LOWEST (lexer/lex "5 != 5;"))
+  (parse-expr LOWEST (lexer/lex "5 !=;"))
   (parse-program (lexer/lex "let hello = 5;"))
   (clojure.pprint/pprint(parse-program (lexer/lex "3 + 4 * 5 == 3 * 1 + 4 * 5")))
   ())
