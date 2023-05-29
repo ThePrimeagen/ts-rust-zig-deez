d0476ee70c47402310662d4fe5152c20371067e2
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-05-29 13:37:02 +0200

    run black code formatter over python version

--- python/lexer/main.py
+++ python/lexer/main.py
@@ -1,7 +1,7 @@
 from lexer import Lexer
 
-if __name__ == '__main__':
-    input = '=+(){},;'
+if __name__ == "__main__":
+    input = "=+(){},;"
     lex = Lexer(input)
     for c in input:
         print(lex.get_next_token())
--- python/lexer/tokens.py
+++ python/lexer/tokens.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from dataclasses import dataclass
 
+
 class TokenType(Enum):
     Illegal = "ILLEGAL"
     Eof = "EOF"
@@ -17,17 +18,19 @@ class TokenType(Enum):
     Function = "FUNCTION"
     Let = "LET"
 
+
 char_to_token_type = {
-    '=': TokenType.Equal,
-    '+': TokenType.Plus,
-    ',': TokenType.Comma,
-    ';': TokenType.Semicolon,
-    '(': TokenType.LParen,
-    ')': TokenType.RParen,
-    '{': TokenType.LSquirly,
-    '}': TokenType.RSquirly,
-    '\x00': TokenType.Eof
+    "=": TokenType.Equal,
+    "+": TokenType.Plus,
+    ",": TokenType.Comma,
+    ";": TokenType.Semicolon,
+    "(": TokenType.LParen,
+    ")": TokenType.RParen,
+    "{": TokenType.LSquirly,
+    "}": TokenType.RSquirly,
+    "\x00": TokenType.Eof,
 }
+
 
 @dataclass
 class Token:
