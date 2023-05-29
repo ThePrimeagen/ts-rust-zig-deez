035537a4ec1db7c4331373eb4d335c58d8db5f30
Luiz Fernando Afra Brito <luizfernandoafrabrito@gmail.com>
2023-05-29 16:17:32 -0300

    reduce one dcg rule

--- prolog/tokenizer.pl
+++ prolog/tokenizer.pl
@@ -3,30 +3,29 @@
 tokenize(StringCodes, Tokens) :- phrase(tok(Tokens), StringCodes).
 
 tok([Token | Ts]) --> ch(punct, [C|Cs]), {lex([C|Cs], Token)}, !, tok(Ts).
-tok([Token | Ts]) --> [C], {lex(C, Token)}, !, tok(Ts).
 tok([Token | Ts]) --> ch(alpha, [C|Cs]), {lex([C|Cs], Token)}, !, tok(Ts).
 tok([int(Number) | Ts]) --> ch(digit, [C|Cs]), {number_codes(Number, [C|Cs])}, !, tok(Ts).
 tok([ident(Atom) | Ts]) --> ch(alpha, [C|Cs]), {atom_codes(Atom, [C|Cs])}, !, tok(Ts).
 tok(Ts) --> ch(space, [_]), !, tok(Ts).
 tok([eof]) --> [].
 
-ch(Type, [C|Cs]) --> [C], {code_type(C, Type)}, !, ch(Type, Cs).
+ch(Type, [C|Cs]) --> [C], {code_type(C, Type)}, ch(Type, Cs).
 ch(_, []) --> [].
 
-lex(61, equal).
-lex(43, plus).
-lex(44, comma).
-lex(59, semicolon).
-lex(40, lparen).
-lex(41, rparen).
-lex(123, lsquirly).
-lex(125, rsquirly).
-lex(33, bang).
-lex(45, dash).
-lex(47, fslash).
-lex(42, asterisk).
-lex(60, lt).
-lex(62, gt).
+lex([61], equal).
+lex([43], plus).
+lex([44], comma).
+lex([59], semicolon).
+lex([40], lparen).
+lex([41], rparen).
+lex([123], lsquirly).
+lex([125], rsquirly).
+lex([33], bang).
+lex([45], dash).
+lex([47], fslash).
+lex([42], asterisk).
+lex([60], lt).
+lex([62], gt).
 lex([61, 61], eq).
 lex([33, 61], neq).
 lex([62, 61], geq).
