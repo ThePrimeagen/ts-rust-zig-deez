3ced798d684ab97d59a0fb1589f0611a8f2d9380
hermannm <hermann.morkrid@gmail.com>
2023-05-31 22:36:03 +0200

    add kotlin repl

--- kotlin2/build.gradle.kts
+++ kotlin2/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0-Beta"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
 
---
+++ kotlin2/src/main/kotlin/Repl.kt
@@ -0,0 +1,15 @@
+﻿package dev.hermannm.monkeylang
+
+fun main() {
+    while (true) {
+        print(">> ")
+        val line = readln()
+        val lexer = Lexer(line)
+
+        var token = lexer.nextToken()
+        while (token != Token.EndOfFile) {
+            println(token)
+            token = lexer.nextToken()
+        }
+    }
+}
