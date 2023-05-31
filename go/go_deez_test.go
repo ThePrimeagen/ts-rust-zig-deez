7d3436f0e0a510438fb88ce8915617c55e7ecbc8
GiorgosAthanasopoulos <spzdoom@gmail.com>
2023-06-01 00:27:00 +0300

    removed repeatable code from tests

--- go/go_deez_test.go
+++ go/go_deez_test.go
@@ -7,14 +7,8 @@ func TestGetNextToken(t *testing.T) {
 
 	var tokens []TokenType
 
-	tokens = append(tokens, Equal)
-	tokens = append(tokens, Plus)
-	tokens = append(tokens, LParen)
-	tokens = append(tokens, RParen)
-	tokens = append(tokens, LSquirly)
-	tokens = append(tokens, RSquirly)
-	tokens = append(tokens, Comma)
-	tokens = append(tokens, Semicolon)
+	tokens = append(tokens, Equal, Plus, LParen, RParen,
+		LSquirly, RSquirly, Comma, Semicolon)
 
 	var lexer = NewTokenizer(input)
 
@@ -38,46 +32,46 @@ func TestGetNextTokenComplete(t *testing.T) {
 
 	var tokens []Token
 
-	tokens = append(tokens, CreateToken(Let, "let"))
-	tokens = append(tokens, CreateToken(Ident, "five"))
-	tokens = append(tokens, CreateToken(Equal, "="))
-	tokens = append(tokens, CreateToken(Int, "5"))
-	tokens = append(tokens, CreateToken(Semicolon, ";"))
+	tokens = append(tokens, CreateToken(Let, "let"),
+		CreateToken(Ident, "five"),
+		CreateToken(Equal, "="),
+		CreateToken(Int, "5"),
+		CreateToken(Semicolon, ";"),
 
-	tokens = append(tokens, CreateToken(Let, "let"))
-	tokens = append(tokens, CreateToken(Ident, "ten"))
-	tokens = append(tokens, CreateToken(Equal, "="))
-	tokens = append(tokens, CreateToken(Int, "10"))
-	tokens = append(tokens, CreateToken(Semicolon, ";"))
+		CreateToken(Let, "let"),
+		CreateToken(Ident, "ten"),
+		CreateToken(Equal, "="),
+		CreateToken(Int, "10"),
+		CreateToken(Semicolon, ";"),
 
-	tokens = append(tokens, CreateToken(Let, "let"))
-	tokens = append(tokens, CreateToken(Ident, "add"))
-	tokens = append(tokens, CreateToken(Equal, "="))
-	tokens = append(tokens, CreateToken(Function, "fn"))
-	tokens = append(tokens, CreateToken(LParen, "("))
-	tokens = append(tokens, CreateToken(Ident, "x"))
-	tokens = append(tokens, CreateToken(Comma, ","))
-	tokens = append(tokens, CreateToken(Ident, "y"))
-	tokens = append(tokens, CreateToken(RParen, ")"))
-	tokens = append(tokens, CreateToken(LSquirly, "{"))
-	tokens = append(tokens, CreateToken(Ident, "x"))
-	tokens = append(tokens, CreateToken(Plus, "+"))
-	tokens = append(tokens, CreateToken(Ident, "y"))
-	tokens = append(tokens, CreateToken(Semicolon, ";"))
-	tokens = append(tokens, CreateToken(RSquirly, "}"))
-	tokens = append(tokens, CreateToken(Semicolon, ";"))
+		CreateToken(Let, "let"),
+		CreateToken(Ident, "add"),
+		CreateToken(Equal, "="),
+		CreateToken(Function, "fn"),
+		CreateToken(LParen, "("),
+		CreateToken(Ident, "x"),
+		CreateToken(Comma, ","),
+		CreateToken(Ident, "y"),
+		CreateToken(RParen, ")"),
+		CreateToken(LSquirly, "{"),
+		CreateToken(Ident, "x"),
+		CreateToken(Plus, "+"),
+		CreateToken(Ident, "y"),
+		CreateToken(Semicolon, ";"),
+		CreateToken(RSquirly, "}"),
+		CreateToken(Semicolon, ";"),
 
-	tokens = append(tokens, CreateToken(Let, "let"))
-	tokens = append(tokens, CreateToken(Ident, "result"))
-	tokens = append(tokens, CreateToken(Equal, "="))
-	tokens = append(tokens, CreateToken(Ident, "add"))
-	tokens = append(tokens, CreateToken(LParen, "("))
-	tokens = append(tokens, CreateToken(Ident, "five"))
-	tokens = append(tokens, CreateToken(Comma, ","))
-	tokens = append(tokens, CreateToken(Ident, "ten"))
-	tokens = append(tokens, CreateToken(RParen, ")"))
-	tokens = append(tokens, CreateToken(Semicolon, ";"))
-	tokens = append(tokens, CreateToken(Eof, "eof"))
+		CreateToken(Let, "let"),
+		CreateToken(Ident, "result"),
+		CreateToken(Equal, "="),
+		CreateToken(Ident, "add"),
+		CreateToken(LParen, "("),
+		CreateToken(Ident, "five"),
+		CreateToken(Comma, ","),
+		CreateToken(Ident, "ten"),
+		CreateToken(RParen, ")"),
+		CreateToken(Semicolon, ";"),
+		CreateToken(Eof, "eof"))
 
 	for _, token := range tokens {
 		var nextToken = lexer.GetNextToken()
