b272e5968b721e96da0dc0828c5670ce6e5d34e0
Alex Jercan <jercan_alex27@yahoo.com>
2023-06-01 03:55:41 +0300

    feat(app): added cmd arguments for different implementations

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,5 +4,28 @@ import Lexer.Basic qualified (tokenize)
 import Lexer.Monad qualified (tokenize)
 import Lexer.State qualified (tokenize)
 
+import System.Environment (getArgs)
+import System.Exit (exitSuccess, exitWith, ExitCode (ExitFailure))
+import Token (Tokenizer)
+
 main :: IO ()
-main = interact $ show . Lexer.Basic.tokenize
+main = getArgs >>= parse >>= \lexer -> interact $ show . chooseLexer lexer
+
+chooseLexer :: String -> Tokenizer
+chooseLexer "basic" = Lexer.Basic.tokenize
+chooseLexer "monad" = Lexer.Monad.tokenize
+chooseLexer "state" = Lexer.State.tokenize
+chooseLexer _       = error "Unknown lexer: use basic, monad, or state"
+
+parse :: [[Char]] -> IO String
+parse ["-h"] = usage >> exitSuccess
+parse ["-v"] = version >> exitSuccess
+parse []     = read <$> getContents
+parse [lexer]  = return lexer
+parse _      = usage >> exitWith (ExitFailure 1)
+
+usage :: IO ()
+usage = putStrLn "Usage: haskell [-vh] <lexer>"
+
+version :: IO ()
+version = putStrLn "Monkey Lexer"
