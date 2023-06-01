3244e7d6b2d7b57808a051649153a7a9d2c50adc
Luiz Fernando Afra Brito <luizfernandoafrabrito@gmail.com>
2023-06-01 08:34:24 -0300

    add repl, rename tokenizer -> lexer

--- prolog/test.pl
+++ prolog/test.pl
@@ -1,22 +1,22 @@
 :- begin_tests(lists).
-:- use_module(tokenizer, [tokenize/2]).
+:- use_module(lexer, [lex/2]).
 
 test(simple) :-
-        tokenize(`=+(){},;`,
-            [equal, plus, lparen, rparen, lsquirly, rsquirly, comma, semicolon, eof]).
+        lex(`=+(){},;`,
+            [assign, plus, lparen, rparen, lsquirly, rsquirly, comma, semicolon, eof]).
 
 test(full) :-
-        tokenize(`
+        lex(`
         let five = 5;
         let ten = 10;
         let add = fn(x, y) {
             x + y;
         };
         let result = add(five, ten); `,
-            [let, ident(five), equal, int(5), semicolon, let, ident(ten), equal, int(10), semicolon, let, ident(add), equal, function, lparen, ident(x), comma, ident(y), rparen, lsquirly, ident(x), plus, ident(y), semicolon, rsquirly, semicolon, let, ident(result), equal, ident(add), lparen, ident(five), comma, ident(ten), rparen, semicolon, eof]).
+            [let, ident(five), assign, int(5), semicolon, let, ident(ten), assign, int(10), semicolon, let, ident(add), assign, function, lparen, ident(x), comma, ident(y), rparen, lsquirly, ident(x), plus, ident(y), semicolon, rsquirly, semicolon, let, ident(result), assign, ident(add), lparen, ident(five), comma, ident(ten), rparen, semicolon, eof]).
 
 test(fullest) :-
-        tokenize(`
+        lex(`
         let five = 5;
         let ten = 10;
         let add = fn(x, y) {
@@ -35,7 +35,7 @@ test(fullest) :-
 
         10 == 100;
         10 != 9; `,
-            [let, ident(five), equal, int(5), semicolon, let, ident(ten), equal, int(10), semicolon, let, ident(add), equal, function, lparen, ident(x), comma, ident(y), rparen, lsquirly, ident(x), plus, ident(y), semicolon, rsquirly, semicolon, let, ident(result), equal, ident(add), lparen, ident(five), comma, ident(ten), rparen, semicolon, bang, dash, fslash, asterisk, int(5), semicolon, int(5), lt, int(10), gt, int(5), semicolon, if, lparen, int(5), lt, int(10), rparen, lsquirly, return, true, semicolon, rsquirly, else, lsquirly, return, false, semicolon, rsquirly, int(10), eq, int(100), semicolon, int(10), neq, int(9), semicolon, eof]).
+            [let, ident(five), assign, int(5), semicolon, let, ident(ten), assign, int(10), semicolon, let, ident(add), assign, function, lparen, ident(x), comma, ident(y), rparen, lsquirly, ident(x), plus, ident(y), semicolon, rsquirly, semicolon, let, ident(result), assign, ident(add), lparen, ident(five), comma, ident(ten), rparen, semicolon, bang, dash, fslash, asterisk, int(5), semicolon, int(5), lt, int(10), gt, int(5), semicolon, if, lparen, int(5), lt, int(10), rparen, lsquirly, return, true, semicolon, rsquirly, else, lsquirly, return, false, semicolon, rsquirly, int(10), eq, int(100), semicolon, int(10), neq, int(9), semicolon, eof]).
 
 
 :- end_tests(lists).
