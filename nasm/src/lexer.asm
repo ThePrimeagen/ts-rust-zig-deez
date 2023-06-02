7dab17763a01ffcdaf5183e7785468c4a26d293b
Ivy Kalliardos <lkalliardos@gmail.com>
2023-06-02 12:41:25 +0200

    Changed 	 to '    '

--- nasm/src/lexer.asm
+++ nasm/src/lexer.asm
@@ -11,19 +11,19 @@
 ; =========================================================================== ;
 %define TOKEN_EOF          0
 
-%define TOKEN_LSQUIRLY     1		; {
-%define TOKEN_RSQUIRLY     2		; }
-%define TOKEN_LPAREN       3		; (
-%define TOKEN_RPAREN       4		; )
+%define TOKEN_LSQUIRLY     1        ; {
+%define TOKEN_RSQUIRLY     2        ; }
+%define TOKEN_LPAREN       3        ; (
+%define TOKEN_RPAREN       4        ; )
 
-%define TOKEN_PLUS         5		; +
-%define TOKEN_DASH         6		; -
-%define TOKEN_COMMA        7		; ,
-%define TOKEN_SEMICOLON    8		; ;
-%define TOKEN_BANG         9		; !
-%define TOKEN_ASTERIKS     10		; *
-%define TOKEN_ASSIGN       11		; =
-%define TOKEN_FORWARDSLASH 12		; /
+%define TOKEN_PLUS         5        ; +
+%define TOKEN_DASH         6        ; -
+%define TOKEN_COMMA        7        ; ,
+%define TOKEN_SEMICOLON    8        ; ;
+%define TOKEN_BANG         9        ; !
+%define TOKEN_ASTERIKS     10       ; *
+%define TOKEN_ASSIGN       11       ; =
+%define TOKEN_FORWARDSLASH 12       ; /
 
 section .text
 
@@ -35,14 +35,14 @@ global lexer_next
 
 ; =========================================================================== ;
 ; args:
-;  - lexer: Lexer*       			offset = 0
-;  - source: char*       			offset = 8
-;  - length: uint64_t    			offset = 16
+;  - lexer: Lexer*                  offset = 0
+;  - source: char*                  offset = 8
+;  - length: uint64_t               offset = 16
 ; =========================================================================== ;
 lexer_init:
-	mov [rdi], rsi             		; lexer->source = source
-	mov [rdi + 8], rdx         		; lexer->length = length
-	mov qword [rdi + 16], 0			; lexer->pos    = 0
+	mov [rdi], rsi                  ; lexer->source = source
+	mov [rdi + 8], rdx              ; lexer->length = length
+	mov qword [rdi + 16], 0         ; lexer->pos    = 0
 
 	ret
 	
@@ -56,9 +56,9 @@ lexer_next:
 
 	; compare lexer->ch
 	movzx edx, byte [rdi + 24]
-	cmp dl, 123                    	; byte code for '{'
+	cmp dl, 123                     ; byte code for '{'
 	je lexer_case_l_squirly
-	cmp dl, 125                    	; byte code for '}'
+	cmp dl, 125                     ; byte code for '}'
 	je lexer_case_r_squirly
 
 lexer_case_l_squirly:
@@ -77,21 +77,21 @@ end:
 ;  - lexer: Lexer*
 ; =========================================================================== ;
 lexer_advance:
-	mov rax, [rdi + 16]				; load the reference of position into rax
-	xor edx, edx					; set edx to 0
+	mov rax, [rdi + 16]             ; load the reference of position into rax
+	xor edx, edx                    ; set edx to 0
 
 	cmp rax, [rdi + 8]
-	jge lexer_advance_write			; jump if pos >= length
+	jge lexer_advance_write         ; jump if pos >= length
 
-	mov rdx, [rdi]			   		; load the address of source into rdx
-	movzx edx, byte [rdx + rax]		; set edx to the char of lexer->source[lexer->read]
+	mov rdx, [rdi]                  ; load the address of source into rdx
+	movzx edx, byte [rdx + rax]     ; set edx to the char of lexer->source[lexer->read]
 
-	add rax, 1						; increment pos
+	add rax, 1                      ; increment pos
 	mov [rdi + 16], rax 
 
 lexer_advance_write:
-	mov [rdi + 24], dl				; load the value of dl into ch. dl is part of edx thus beeing 0,
-									; if not overwritten. This happens if pos >= length
+	mov [rdi + 24], dl              ; load the value of dl into ch. dl is part of edx thus beeing 0,
+	                                ; if not overwritten. This happens if pos >= length
 	ret
 
 ; =========================================================================== ;
