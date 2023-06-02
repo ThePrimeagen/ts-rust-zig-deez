c17ab2ce884503cba63ac02cf28419e63599313a
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2023-06-02 19:27:27 +0530

    dart: add a repl

--- dart/bin/dart_deez.dart
+++ dart/bin/dart_deez.dart
@@ -1,10 +1,37 @@
 import 'package:dart_deez/dart_deez.dart';
 import 'dart:io';
 
+String yellow(String s) => "\x1b[33m$s\x1b[0m";
+
+Never repl() {
+  final prompt = yellow("(deez)>> ");
+
+  while (true) {
+    stdout.write(prompt);
+    final line = stdin.readLineSync();
+
+    if (line == null) {
+      break;
+    }
+
+    final lexer = Lexer(line);
+
+    for (final tok in lexer.iter()) {
+      print(tok);
+    }
+  }
+
+  exit(0);
+}
+
 void main(List<String> arguments) async {
   if (arguments.isEmpty) {
-    print("Usage: dart run bin/main.dart <filename>");
-    return;
+    print(
+      "You are now in REPL mode because you didn't specify a filename.\n"
+      "If you want to run a file, specify it as an argument. like so:\n"
+      "Press Ctrl+C to exit.\n",
+    );
+    repl();
   }
 
   final fileName = arguments[0];
