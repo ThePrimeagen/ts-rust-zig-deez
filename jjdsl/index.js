6c38da9d06bdfa4fc7df920f904812a04713053c
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2023-06-03 09:25:36 -0600

    Added Lexer Function Definitions

--- jjdsl/index.js
+++ jjdsl/index.js
@@ -1,12 +1,33 @@
 // Classes
 
-function Lexer(input) {
-    this.input = input;
-    this.position = 0;
-    this.ch = this.input[this.position];
+Lexer.prototype.skipWhiteSpace() = function() {
+
 }
 
-function Token(tokenType, value) {
-    this.tokenType = tokenType;
-    this.value = value;
+Lexer.prototype.readInt() = function() {
+
+}
+
+Lexer.prototype.readIdent() = function () {
+
+}
+
+Lexer.prototype.readChar() = function() {
+
+}
+
+Lexer.prototype.isAlpha(x) = function(x) {
+
+}
+
+Lexer.prototype.isNumeric(x) = function(x) {
+
+}
+
+Lexer.prototype.lookupIdent(x) = function(x) {
+
+}
+
+Lexer.prototype.nextToken() = function() {
+
 }
