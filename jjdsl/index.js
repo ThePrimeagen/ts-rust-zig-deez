68ab8e0021c7f0c8865c13376056b2389e3e5e2d
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2023-06-03 11:35:23 -0600

    Added Lexer Functions

--- jjdsl/index.js
+++ jjdsl/index.js
@@ -1,7 +1,69 @@
-function isSingleWidthToken(x) {
-    if (x === "=" || x === "+" || x === "," || x === ";" || x === "(" || x === ")" || x === "{" || x === "}") {
-        return new Token(x, undefined);
-    } else {
-        return null;
+Lexer.prototype.readChar = function () {
+    this.position++;
+    this.ch = this.position >= this.input.length ? "" : this.input[this.position];
+}
+
+Lexer.prototype.isAlpha = function (x) {
+    return x >= "a" && x <= "z" || x >= "A" && x <= "Z" || x === "_";
+}
+
+Lexer.prototype.isNumeric = function (x) {
+    return x >= "0" && x <= "9";
+}
+
+
+Lexer.prototype.skipWhitespace = function () {
+    while (this.ch === " " || this.ch === "\t" || this.ch === "\n" || this.ch === "\r") {
+        this.readChar();
     }
+}
+
+Lexer.prototype.readIdent = function () {
+    let pos = this.position;
+    while (this.isAlpha(this.ch)) {
+        this.readChar();
+    }
+    return this.input.slice(pos, this.position);
+}
+
+Lexer.prototype.readInt = function () {
+    let pos = this.position;
+    while (this.isNumeric(this.ch)) {
+        this.readChar();
+    }
+    return this.input.slice(pos, this.position);
+}
+
+Lexer.prototype.lookUpIdent = function (x) {
+    if (x === "fn") {
+        return new Token("FUNCTION", undefined);
+    } else if (x === "let") {
+        return new Token("LET", undefined);
+    } else {
+        return new Token("IDENT", x);
+    }
+}
+
+Lexer.prototype.nextToken = function() {
+    this.skipWhitespace();
+
+    let token = isSingleWidthToken(this.ch);
+
+    if (token === null) {
+        if (this.isAlpha(this.ch)) {
+            let ident = this.readIdent();
+            token = this.lookupIdent(ident);
+        }
+
+        if (this.isNumeric(this.ch)) {
+            let intVal = this.readInt();
+            token = new Token("INT", intVal);
+        }
+
+        token = this.ch === "" ? new Token("EOF", "") : new Token("ILLEGAL", null);
+    }
+
+    this.readChar();
+
+    return token;
 }
