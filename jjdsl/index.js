7032f131feb73dc4febd8d35a4ccb6d988b47c8b
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2023-06-03 12:30:14 -0600

    Token Helper Functions

--- jjdsl/index.js
+++ jjdsl/index.js
@@ -1,69 +1,10 @@
-Lexer.prototype.readChar = function () {
-    this.position++;
-    this.ch = this.position >= this.input.length ? "" : this.input[this.position];
-}
-
-Lexer.prototype.isAlpha = function (x) {
-    return x >= "a" && x <= "z" || x >= "A" && x <= "Z" || x === "_";
-}
-
-Lexer.prototype.isNumeric = function (x) {
-    return x >= "0" && x <= "9";
-}
-
-
-Lexer.prototype.skipWhitespace = function () {
-    while (this.ch === " " || this.ch === "\t" || this.ch === "\n" || this.ch === "\r") {
-        this.readChar();
+Token.prototype.softCheck = function(x) {
+    if (this.tokenType !== x.tokenType) {
+        return false
     }
+    return this.value === x.value;
 }
 
-Lexer.prototype.readIdent = function () {
-    var pos = this.position;
-    while (this.isAlpha(this.ch)) {
-        this.readChar();
-    }
-    return this.input.slice(pos, this.position);
-}
-
-Lexer.prototype.readInt = function () {
-    var pos = this.position;
-    while (this.isNumeric(this.ch)) {
-        this.readChar();
-    }
-    return this.input.slice(pos, this.position);
-}
-
-Lexer.prototype.lookUpIdent = function (x) {
-    if (x === "fn") {
-        return new Token("FUNCTION", undefined);
-    } else if (x === "let") {
-        return new Token("LET", undefined);
-    } else {
-        return new Token("IDENT", x);
-    }
-}
-
-Lexer.prototype.nextToken = function() {
-    this.skipWhitespace();
-
-    var token = isSingleWidthToken(this.ch);
-
-    if (token === null) {
-        if (this.isAlpha(this.ch)) {
-            var ident = this.readIdent();
-            token = this.lookupIdent(ident);
-        }
-
-        if (this.isNumeric(this.ch)) {
-            var intVal = this.readInt();
-            token = new Token("INT", intVal);
-        }
-
-        token = this.ch === "" ? new Token("EOF", "") : new Token("ILLEGAL", null);
-    }
-
-    this.readChar();
-
-    return token;
+Token.prototype.readable = function() {
+    return "TokenType: " + this.tokenType + " | Value: " + this.value;
 }
