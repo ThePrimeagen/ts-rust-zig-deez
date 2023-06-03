5b6d62abce322cdb13d0da0b1bd4f7aaf4d86808
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2023-06-03 11:34:14 -0600

    Added Simple Token Scan

--- jjdsl/index.js
+++ jjdsl/index.js
@@ -1,33 +1,7 @@
-// Classes
-
-Lexer.prototype.skipWhiteSpace() = function() {
-
-}
-
-Lexer.prototype.readInt() = function() {
-
-}
-
-Lexer.prototype.readIdent() = function () {
-
-}
-
-Lexer.prototype.readChar() = function() {
-
-}
-
-Lexer.prototype.isAlpha(x) = function(x) {
-
-}
-
-Lexer.prototype.isNumeric(x) = function(x) {
-
-}
-
-Lexer.prototype.lookupIdent(x) = function(x) {
-
-}
-
-Lexer.prototype.nextToken() = function() {
-
+function isSingleWidthToken(x) {
+    if (x === "=" || x === "+" || x === "," || x === ";" || x === "(" || x === ")" || x === "{" || x === "}") {
+        return new Token(x, undefined);
+    } else {
+        return null;
+    }
 }
