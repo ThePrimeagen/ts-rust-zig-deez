093a07c7af20de30cf01f7344ca7938cf2d73f22
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2023-06-03 12:31:21 -0600

    Test Suite

--- jjdsl/index.js
+++ jjdsl/index.js
@@ -1,68 +1,82 @@
-Lexer.prototype.readChar = function () {
-    this.position++;
-    this.ch = this.position >= this.input.length ? "" : this.input[this.position];
-}
-
-Lexer.prototype.isAlpha = function (x) {
-    return x >= "a" && x <= "z" || x >= "A" && x <= "Z" || x === "_";
-}
-
-Lexer.prototype.isNumeric = function (x) {
-    return x >= "0" && x <= "9";
-}
-
-
-Lexer.prototype.skipWhitespace = function () {
-    while (this.ch === " " || this.ch === "\t" || this.ch === "\n" || this.ch === "\r") {
-        this.readChar();
-    }
-}
-
-Lexer.prototype.readIdent = function () {
-    var pos = this.position;
-    while (this.isAlpha(this.ch)) {
-        this.readChar();
-    }
-    return this.input.slice(pos, this.position);
-}
-
-Lexer.prototype.readInt = function () {
-    var pos = this.position;
-    while (this.isNumeric(this.ch)) {
-        this.readChar();
-    }
-    return this.input.slice(pos, this.position);
-}
-
-Lexer.prototype.lookUpIdent = function (x) {
-    if (x === "fn") {
-        return new Token("FUNCTION", undefined);
-    } else if (x === "let") {
-        return new Token("LET", undefined);
-    } else {
-        return new Token("IDENT", x);
-    }
-}
-
-Lexer.prototype.nextToken = function () {
-    this.skipWhitespace();
-
-    var token = isSingleWidthToken(this.ch);
-
-    if (token === null) {
-        if (this.isAlpha(this.ch)) {
-            var ident = this.readIdent();
-            return this.lookUpIdent(ident);
+function test_next_token_function() {
+    var basic_tokens = "=+(){},;";
+    var lexer = new Lexer(basic_tokens);
+    var expected_tokens = ["=", "+", "(", ")", "{", "}", ",", ";"];
+    for (var index = 0; index < expected_tokens.length; index++) {
+        var next_token = lexer.nextToken().tokenType;
+        if (next_token !== expected_tokens[index]) {
+            return "FAILED TO PARSE TOKEN: " + next_token + " EXPECTED: " + expected_tokens[index];
         }
-        if (this.isNumeric(this.ch)) {
-            var intVal = this.readInt();
-            return new Token("INT", intVal);
-        }
-        token = this.ch === "" ? new Token("EOF", "") : new Token("ILLEGAL", null);
-
     }
+    return null;
+}
 
-    this.readChar();
+function test_get_next_complete() {
+    var input = "let five = 5;" +
+        "let ten = 10;" +
+        "let add = fn(x, y) {" +
+        "x + y;" +
+        "};" +
+        "let result = add(five, ten);";
 
-    return token;
+    var lexer = new Lexer(input);
+
+    var expected_tokens = [
+        new Token("LET", undefined),
+        new Token("IDENT", "five"),
+        new Token("=", undefined),
+        new Token("INT", "5"),
+        new Token(";", undefined),
+        new Token("LET", undefined),
+        new Token("IDENT", "ten"),
+        new Token("=", undefined),
+        new Token("INT", "10"),
+        new Token(";"),
+        new Token("LET"),
+        new Token("IDENT", "add"),
+        new Token("=", undefined),
+        new Token("FUNCTION", undefined),
+        new Token("(", undefined),
+        new Token("IDENT", "x"),
+        new Token(",", undefined),
+        new Token("IDENT", "y"),
+        new Token(")", undefined),
+        new Token("{", undefined),
+        new Token("IDENT", "x"),
+        new Token("+", undefined),
+        new Token("IDENT", "y"),
+        new Token(";", undefined),
+        new Token("}", undefined),
+        new Token(";", undefined),
+        new Token("LET", undefined),
+        new Token("IDENT", "result"),
+        new Token("=", undefined),
+        new Token("IDENT", "add"),
+        new Token("(", undefined),
+        new Token("IDENT", "five"),
+        new Token(",", undefined),
+        new Token("IDENT", "ten"),
+        new Token(")", undefined),
+        new Token(";", undefined),
+        new Token("EOF", "")
+    ];
+    for (var index = 0; index < expected_tokens.length; index++) {
+        var next_token = lexer.nextToken();
+        if (!next_token.softCheck(expected_tokens[index])) {
+            return "FAILED TO PARSE TOKEN: " + next_token.readable() + " EXPECTED [" + index + "]: " + expected_tokens[index].readable();
+        }
+    }
+    return null;
+}
+
+function test() {
+    var results = test_next_token_function();
+    if (results !== null) {
+        return results;
+    }
+    results = test_get_next_complete();
+    if (results !== null) {
+        return results;
+    }
+    return "Tests Passed";
 }
