9b9b0664c772ba2080103c72efa8cceba0ff0e10
asafbennatan <asafbennatan@gmail.com>
2023-06-03 18:14:39 +0300

    using pattern matching instead of map , using singleton Tokens for literals (#153)
    Co-authored-by: Asaf Ben Natan <asaf@wizzdi.com>

--- java/monkey/src/main/java/Lexer.java
+++ java/monkey/src/main/java/Lexer.java
@@ -1,17 +1,9 @@
-import java.util.HashMap;
-import java.util.Optional;
+
 
 public class Lexer {
 
     private final String input;
     private int pos = 0;
-
-    final HashMap<String, Token> keywordMap = new HashMap<>() {
-        {
-            put("fn", TokenType.FUNC.createToken("fn"));
-            put("let", TokenType.LET.createToken("let"));
-        }
-    };
 
     public Lexer(String input) {
         this.input = input;
@@ -20,41 +12,42 @@ public class Lexer {
     public Token nextToken() {
         this.skipWhitespace();
 
-        var tokenType = switch (this.getCc()) {
-            case '{' -> TokenType.LSQIRLY;
-            case '}' -> TokenType.RSQIRLY;
-            case '(' -> TokenType.LPAREN;
-            case ')' -> TokenType.RPAREN;
-            case ',' -> TokenType.COMMA;
-            case ';' -> TokenType.SEMI;
-            case '+' -> TokenType.PLUS;
-            case '=' -> TokenType.EQUAL;
-            case '\0' -> TokenType.EOF;
-            default -> TokenType.ILLEGAL;
+        var token = switch (this.getCc()) {
+            case '{' -> TokenType.LSQIRLY.token();
+            case '}' -> TokenType.RSQIRLY.token();
+            case '(' -> TokenType.LPAREN.token();
+            case ')' -> TokenType.RPAREN.token();
+            case ',' -> TokenType.COMMA.token();
+            case ';' -> TokenType.SEMI.token();
+            case '+' -> TokenType.PLUS.token();
+            case '=' -> TokenType.EQUAL.token();
+            case '\0' -> TokenType.EOF.token();
+            default -> TokenType.ILLEGAL.createToken(String.valueOf(this.getCc()));
         };
 
-        var token = switch (tokenType) {
-            case EOF -> tokenType.createToken("eof");
-            default -> tokenType.createToken(String.valueOf(this.getCc()));
-        };
 
         if (token.type() != TokenType.ILLEGAL) {
             this.advance();
             return token;
         }
+       switch ((Character)this.getCc()){
+            case Character c when isLetter(c) -> {
+                var ident = this.indent();
+                return switch (ident) {
+                    case "fn" ->  TokenType.FUNC.token();
+                    case "let" ->  TokenType.LET.token();
+                    default ->  TokenType.IDENT.createToken(ident);
+                };
+            }
+            case Character c when Character.isDigit(c) -> {
+                return TokenType.INT.createToken(this.number());
+            }
+           default -> {
+               this.advance();
+               return token;
+           }
+       }
 
-        if (this.isLetter(this.getCc())) {
-            var ident = this.indent();
-
-            return Optional
-                    .ofNullable(this.keywordMap.get(ident))
-                    .orElseGet(() -> TokenType.IDENT.createToken(ident));
-        } else if (Character.isDigit(this.getCc())) {
-            return TokenType.INT.createToken(this.number());
-        }
-
-        this.advance();
-        return token;
     }
 
     private void advance() {
