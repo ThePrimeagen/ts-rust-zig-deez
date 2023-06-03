9ec540634af11591ca8f067cea78467ecae16ecc
Spongman <1088194+Spongman@users.noreply.github.com>
2023-06-03 00:22:09 -0700

    cpp: rewrite? (#99)
    * rewrite?
    
    * add repl

---
+++ cpp/src/lexer/lexer.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string_view>
+
+#include "token.hpp"
+
+class Lexer final
+{
+	public:
+		Lexer(std::string_view);
+		void next() noexcept;
+		Token fetch(TokenType type);
+		bool get(TokenType type);
+
+		auto type() const noexcept { return token_.type; }
+		auto literal() const noexcept { return token_.literal; }
+
+		const Token& peek() const noexcept { return token_; }
+		bool peekIs(TokenType tokenType) const noexcept { return type() == tokenType; }
+		bool eof() const noexcept { return peekIs(TokenType::Eof); }
+
+	private:
+		Token nextToken() noexcept;
+		bool swallow(char ch) noexcept;
+
+		Token token_;
+
+		const std::string_view input_;
+		std::string_view::iterator position_;
+};
