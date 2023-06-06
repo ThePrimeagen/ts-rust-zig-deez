0e0ec56f9cbc2bd8947f24095665935e2c8d5771
Amis Shokoohi <amisshokoohi@gmail.com>
2023-06-06 17:34:56 +0000

    Squashed commit of the following:
    commit 8d3734199086241037440f1065b520a24ce2884f
    Author: Amis Shokoohi <amisshokoohi@gmail.com>
    Date:   Tue Jun 6 17:33:48 2023 +0000
    
        Add more tokens
    
    commit e0e42904f71278d0e1a3fe0f731a2cbba1658d67
    Author: Amis Shokoohi <amisshokoohi@gmail.com>
    Date:   Mon Jun 5 20:18:56 2023 +0000
    
        Rename brace to squirly

--- go-concurrent/lexer/lexer_test.go
+++ go-concurrent/lexer/lexer_test.go
@@ -4,82 +4,139 @@ import (
 	"testing"
 )
 
-func TestLexerSimple(t *testing.T) {
-	input := "=+(){},;"
-
-	expect := []token{
-		{tokenAssign, "="},
-		{tokenPlus, "+"},
-		{tokenLParen, "("},
-		{tokenRParen, ")"},
-		{tokenLBrace, "{"},
-		{tokenRBrace, "}"},
-		{tokenComma, ","},
-		{tokenSemicolon, ";"},
-	}
-
-	_, tokens := NewLexer(input)
-
-	for _, expectedToken := range expect {
-		if token := <-tokens; token != expectedToken {
-			t.Errorf("expected %v; got %v", expectedToken, token)
-		}
-	}
-}
-
 func TestLexer(t *testing.T) {
-	input := `let five = 5;
-	let ten = 10;
-	let add = fn(x, y) {
-		x + y;
-	};
-	let result = add(five, ten);`
-
-	expect := []token{
-		{tokenLet, "let"},
-		{tokenIdent, "five"},
-		{tokenAssign, "="},
-		{tokenInt, "5"},
-		{tokenSemicolon, ";"},
-		{tokenLet, "let"},
-		{tokenIdent, "ten"},
-		{tokenAssign, "="},
-		{tokenInt, "10"},
-		{tokenSemicolon, ";"},
-		{tokenLet, "let"},
-		{tokenIdent, "add"},
-		{tokenAssign, "="},
-		{tokenFunction, "fn"},
-		{tokenLParen, "("},
-		{tokenIdent, "x"},
-		{tokenComma, ","},
-		{tokenIdent, "y"},
-		{tokenRParen, ")"},
-		{tokenLBrace, "{"},
-		{tokenIdent, "x"},
-		{tokenPlus, "+"},
-		{tokenIdent, "y"},
-		{tokenSemicolon, ";"},
-		{tokenRBrace, "}"},
-		{tokenSemicolon, ";"},
-		{tokenLet, "let"},
-		{tokenIdent, "result"},
-		{tokenAssign, "="},
-		{tokenIdent, "add"},
-		{tokenLParen, "("},
-		{tokenIdent, "five"},
-		{tokenComma, ","},
-		{tokenIdent, "ten"},
-		{tokenRParen, ")"},
-		{tokenSemicolon, ";"},
-		{tokenEOF, ""},
+	cases := []struct {
+		input  string
+		expect []token
+	}{
+		{
+			input: `=+(){},;`,
+			expect: []token{
+				{tokenAssign, "="},
+				{tokenPlus, "+"},
+				{tokenLParen, "("},
+				{tokenRParen, ")"},
+				{tokenLSquirly, "{"},
+				{tokenRSquirly, "}"},
+				{tokenComma, ","},
+				{tokenSemicolon, ";"},
+				{tokenEOF, ""},
+			},
+		},
+		{
+			input: `let five = 5;
+			let ten = 10;
+			let add = fn(x, y) {
+				x + y;
+			};
+			let result = add(five, ten);`,
+			expect: []token{
+				{tokenLet, "let"},
+				{tokenIdent, "five"},
+				{tokenAssign, "="},
+				{tokenInt, "5"},
+				{tokenSemicolon, ";"},
+				{tokenLet, "let"},
+				{tokenIdent, "ten"},
+				{tokenAssign, "="},
+				{tokenInt, "10"},
+				{tokenSemicolon, ";"},
+				{tokenLet, "let"},
+				{tokenIdent, "add"},
+				{tokenAssign, "="},
+				{tokenFunction, "fn"},
+				{tokenLParen, "("},
+				{tokenIdent, "x"},
+				{tokenComma, ","},
+				{tokenIdent, "y"},
+				{tokenRParen, ")"},
+				{tokenLSquirly, "{"},
+				{tokenIdent, "x"},
+				{tokenPlus, "+"},
+				{tokenIdent, "y"},
+				{tokenSemicolon, ";"},
+				{tokenRSquirly, "}"},
+				{tokenSemicolon, ";"},
+				{tokenLet, "let"},
+				{tokenIdent, "result"},
+				{tokenAssign, "="},
+				{tokenIdent, "add"},
+				{tokenLParen, "("},
+				{tokenIdent, "five"},
+				{tokenComma, ","},
+				{tokenIdent, "ten"},
+				{tokenRParen, ")"},
+				{tokenSemicolon, ";"},
+				{tokenEOF, ""},
+			},
+		},
+		{
+			input: `!-/*5;
+			5 < 10 > 5;
+			
+			if (5 < 10) {
+				return true;
+			} else {
+				return false;
+			}
+			
+			10 == 10;
+			10 != 9;`,
+			expect: []token{
+				{tokenBang, "!"},
+				{tokenMinus, "-"},
+				{tokenSlash, "/"},
+				{tokenAsterisk, "*"},
+				{tokenInt, "5"},
+				{tokenSemicolon, ";"},
+				{tokenInt, "5"},
+				{tokenLessThan, "<"},
+				{tokenInt, "10"},
+				{tokenGreaterThan, ">"},
+				{tokenInt, "5"},
+				{tokenSemicolon, ";"},
+				{tokenIf, "if"},
+				{tokenLParen, "("},
+				{tokenInt, "5"},
+				{tokenLessThan, "<"},
+				{tokenInt, "10"},
+				{tokenRParen, ")"},
+				{tokenLSquirly, "{"},
+				{tokenReturn, "return"},
+				{tokenTrue, "true"},
+				{tokenSemicolon, ";"},
+				{tokenRSquirly, "}"},
+				{tokenElse, "else"},
+				{tokenLSquirly, "{"},
+				{tokenReturn, "return"},
+				{tokenFalse, "false"},
+				{tokenSemicolon, ";"},
+				{tokenRSquirly, "}"},
+				{tokenInt, "10"},
+				{tokenEqual, "=="},
+				{tokenInt, "10"},
+				{tokenSemicolon, ";"},
+				{tokenInt, "10"},
+				{tokenNotEqual, "!="},
+				{tokenInt, "9"},
+				{tokenSemicolon, ";"},
+				{tokenEOF, ""},
+			},
+		},
 	}
 
-	_, tokens := NewLexer(input)
+	for _, c := range cases {
+		c := c
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
 
-	for _, expectedToken := range expect {
-		if token := <-tokens; token != expectedToken {
-			t.Errorf("expected %v; got %v", expectedToken, token)
-		}
+			_, tokens := NewLexer(c.input)
+
+			for _, expectedToken := range c.expect {
+				if token := <-tokens; token != expectedToken {
+					t.Errorf("expected %v; got %v", expectedToken, token)
+				}
+			}
+		})
 	}
 }
