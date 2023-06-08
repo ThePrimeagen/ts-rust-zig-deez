af8f33e7060535cee085e0313b94301ab46ca3dd
Mats E. Mollestad <mats@mollestad.no>
2023-06-08 21:03:27 +0200

    chore: Refactoring the lexer

--- swift/Sources/Lexer/token.swift
+++ swift/Sources/Lexer/token.swift
@@ -1,10 +1,62 @@
-import Foundation
 
-enum TokenType {
-    case illegal, eof, ident, int, comma, semi, plus, equal, lParen, rParen, lSqirly, rSqirly, function, `let`
-}
+enum Token: Equatable {
+    case illegal(String)
+    case ident(String)
+    case int(Int)
+    case eof
+    case comma
+    case semi
+    case plus
+    case minus
+    case bang
+    case asterisk
+    case slash
+    case equals
+    case notEquals
+    case assign
+    case `true`
+    case `false`
+    case `if`
+    case `else`
+    case `return`
+    case lParen
+    case rParen
+    case lSqirly
+    case rSqirly
+    case function
+    case `let`
 
-struct Token {
-    let type: TokenType
-    let literal: String
+    var literal: String {
+        switch self {
+            case .illegal(let literal): return literal
+            case .ident(let literal): return literal
+            case .int(let literal): return String(literal)
+            case .eof: return "end of file"
+            case .comma: return ","
+            case .semi: return ";"
+            case .plus: return "+"
+            case .assign: return "="
+            case .lParen: return "("
+            case .rParen: return ")"
+            case .lSqirly: return "{"
+            case .rSqirly: return "}"
+            case .function: return "fn"
+            case .let: return "let"
+            case .minus: return "-"
+            case .bang: return "!"
+            case .asterisk: return "*"
+            case .slash: return "/"
+            case .equals: return "=="
+            case .notEquals: return "!="
+            case .true: return "true"
+            case .false: return "false"
+            case .if: return "if"
+            case .else: return "else"
+            case .return: return "return"
+        }
+    }
+
+    static func ==(lhs: Token, rhs: Token) -> Bool {
+        return lhs.literal == rhs.literal
+    }
 }
