318f665bebab2548eaa1920c2531192d84d0eae9
Paul Erickson <facshus@gmail.com>
2023-06-09 12:53:01 -0500

    feat: Parser compete and tested through page 78 (#216)
    * Dart w/ functional programming through Lexer
    
    * shortened name and removed file headers
    
    * remove the verbose directory name
    
    * adding dart tools to gitignore
    
    * deleting dart tools to gitignore
    
    * resolved merge conflicts
    
    * parser working through pg 77

---
+++ dart-funk/lib/src/precedence.dart
@@ -0,0 +1,44 @@
+import 'package:monkeydart/monkeydart.dart';
+
+enum Precedence {
+  // ignore: unused_field
+  _, // not a real precedent
+  lowest,
+  equals, // ==
+  lessGreater, // > or <
+  sum, // +
+  product, // *
+  prefix, // -X or !X
+  call, // myFunction(X)
+}
+
+Map<TokenType, Precedence> precedences() {
+  return {
+    TokenType.eq: Precedence.equals,
+    TokenType.ne: Precedence.equals,
+    TokenType.lt: Precedence.lessGreater,
+    TokenType.gt: Precedence.lessGreater,
+    TokenType.plus: Precedence.sum,
+    TokenType.dash: Precedence.sum,
+    TokenType.slash: Precedence.product,
+    TokenType.asterisk: Precedence.product,
+  };
+}
+
+Precedence currentPrecedence(Parser parser) {
+  return precedences()[parser.currToken.type] ?? Precedence.lowest;
+}
+
+Precedence peekPrecedence(Parser parser) {
+  return precedences()[parser.peekToken.type] ?? Precedence.lowest;
+}
+
+extension PrecedenceExt on Precedence {
+  bool operator >=(Precedence other) {
+    return index >= other.index;
+  }
+
+  bool operator <(Precedence other) {
+    return index < other.index;
+  }
+}
