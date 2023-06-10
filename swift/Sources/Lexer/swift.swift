0e489fd90fde2cc4bfd5fe71710aaa4476343357
Myles La Verne Schultz <myleslschultz@icloud.com>
2023-06-10 03:08:25 -0400

    Comment removed
    Comment marks placed when testing were removed.

--- swift/Sources/Lexer/swift.swift
+++ swift/Sources/Lexer/swift.swift
@@ -2,9 +2,9 @@ import ArgumentParser
 
 @main
 struct LexerCli: ParsableCommand {
-//   public static let configuration = CommandConfiguration(
-//       abstract: "A Swift command-line tool to lex a string."
-//   )
+   public static let configuration = CommandConfiguration(
+       abstract: "A Swift command-line tool to lex a string."
+   )
 
     mutating func run() {}
 }
