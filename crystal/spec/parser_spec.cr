7f9be18fb33b4df9b816be2c15a99f5ec55c7e25
devnote-dev <devnote.dev75@gmail.com>
2023-06-10 18:12:05 +0100

    feat(crystal): add if expression specs

--- crystal/spec/parser_spec.cr
+++ crystal/spec/parser_spec.cr
@@ -48,117 +48,117 @@ describe Parser do
     describe FunctionLiteral do
       it "parses anonymous functions" do
         statements = parse("fn() {};")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters.should be_empty
         expr.body.statements.should be_empty
       end
-  
+
       it "parses bound functions" do
         statements = parse("let noop = fn() {};")
         statements[0].should be_a Let
-  
+
         let = statements[0].as(Let)
         let.name.should eq Identifier.new("noop")
         let.value.should be_a ExpressionStatement
-  
+
         expr = let.value.as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters.should be_empty
         expr.body.statements.should be_empty
       end
-  
+
       it "parses function return statements" do
         statements = parse("let noop = fn() { return; };")
         statements[0].should be_a Let
-  
+
         expr = statements[0].as(Let).value.as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters.should be_empty
         expr.body.statements[0].should be_a Return
         expr.body.statements[0].as(Return).value.should be_nil
-  
+
         statements = parse("let zero = fn() { return 0; };")
         expr = statements[0].as(Let).value.as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters.should be_empty
         expr.body.statements[0].should be_a Return
         expr.body.statements[0].as(Return).value.should be_a IntegerLiteral
         expr.body.statements[0].as(Return).value.as(IntegerLiteral).value.should eq 0
       end
-  
+
       it "parses function arguments" do
         statements = parse("let foo = fn(x, y) {};")
         statements[0].should be_a Let
-  
+
         expr = statements[0].as(Let).value.as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters.size.should eq 2
         expr.parameters[0].should eq Identifier.new("x")
         expr.parameters[1].should eq Identifier.new("y")
         expr.body.statements.should be_empty
       end
-  
+
       it "parses calls in function bodies" do
         statements = parse("let foo = fn(x) { return bar(x); };")
         statements[0].should be_a Let
-  
+
         expr = statements[0].as(Let).value.as(ExpressionStatement).expression
         expr.should be_a FunctionLiteral
-  
+
         expr = expr.as(FunctionLiteral)
         expr.parameters[0].should eq Identifier.new("x")
         expr.body.statements[0].should be_a Return
         expr.body.statements[0].as(Return).value.should be_a Call
-  
+
         call = expr.body.statements[0].as(Return).value.as(Call)
         call.function.should eq Identifier.new("bar")
         call.arguments[0].should eq Identifier.new("x")
       end
     end
-  
+
     describe Call do
       it "parses no arguments" do
         statements = parse("print();")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a Call
-  
+
         call = expr.as(Call)
         call.function.should eq Identifier.new("print")
         call.arguments.should be_empty
       end
-  
+
       it "parses single arguments" do
         statements = parse("print(123);")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a Call
-  
+
         call = expr.as(Call)
         call.function.should eq Identifier.new("print")
         call.arguments.size.should eq 1
         call.arguments[0].should be_a IntegerLiteral
         call.arguments[0].as(IntegerLiteral).value.should eq 123
       end
-  
+
       it "parses multiple arguments" do
         statements = parse("print(123, 456, 789);")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a Call
-  
+
         call = expr.as(Call)
         call.function.should eq Identifier.new("print")
         call.arguments.size.should eq 3
@@ -170,33 +170,33 @@ describe Parser do
         call.arguments[2].as(IntegerLiteral).value.should eq 789
       end
     end
-  
+
     describe Prefix do
       it "parses signed-expressions" do
         statements = parse("-23")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a Prefix
-  
+
         prefix = expr.as(Prefix)
         prefix.operator.should eq Prefix::Operator::Negative
         prefix.right.should be_a IntegerLiteral
         prefix.right.as(IntegerLiteral).value.should eq 23
       end
-  
+
       it "parses not-expressions" do
         statements = parse("!true")
-  
+
         expr = statements[0].as(ExpressionStatement).expression
         expr.should be_a Prefix
-  
+
         prefix = expr.as(Prefix)
         prefix.operator.should eq Prefix::Operator::Not
         prefix.right.should be_a BooleanLiteral
         prefix.right.as(BooleanLiteral).value?.should be_true
       end
     end
-  
+
     describe Infix do
       {% for op, type in {
                            "+" => "Add",
@@ -206,10 +206,10 @@ describe Parser do
                          } %}
         it "parses {{ type.downcase.id }} operators" do
           statements = parse("6 {{ op.id }} 4;")
-  
+
           expr = statements[0].as(ExpressionStatement).expression
           expr.should be_a Infix
-  
+
           infix = expr.as(Infix)
           infix.left.should be_a IntegerLiteral
           infix.left.as(IntegerLiteral).value.should eq 6
@@ -220,7 +220,61 @@ describe Parser do
       {% end %}
     end
 
-    pending If do
+    describe If do
+      it "parses if expressions" do
+        statements = parse("if (true) { 12 } else { 34 };")
+
+        expr = statements[0].as(ExpressionStatement).expression
+        expr.should be_a If
+
+        expr = expr.as(If)
+        expr.condition.should be_a BooleanLiteral
+        expr.condition.as(BooleanLiteral).value?.should be_true
+        expr.consequence.should be_a Block
+
+        block = expr.consequence.as(Block)
+        block.statements.size.should eq 1
+
+        value = block.statements[0].as(ExpressionStatement).expression
+        value.should be_a IntegerLiteral
+        value.as(IntegerLiteral).value.should eq 12
+
+        expr.alternative.should be_a Block
+
+        block = expr.alternative.as(Block)
+        block.statements.size.should eq 1
+
+        value = block.statements[0].as(ExpressionStatement).expression
+        value.should be_a IntegerLiteral
+        value.as(IntegerLiteral).value.should eq 34
+      end
+
+      it "parses if expressions without alternatives" do
+        statements = parse("if (1 == 1) { false };")
+
+        expr = statements[0].as(ExpressionStatement).expression
+        expr.should be_a If
+
+        expr = expr.as(If)
+        expr.condition.should be_a Infix
+
+        infix = expr.condition.as(Infix)
+        infix.left.should be_a IntegerLiteral
+        infix.left.as(IntegerLiteral).value.should eq 1
+        infix.right.should be_a IntegerLiteral
+        infix.right.as(IntegerLiteral).value.should eq 1
+
+        expr.consequence.should be_a Block
+
+        block = expr.consequence.as(Block)
+        block.statements.size.should eq 1
+
+        value = block.statements[0].as(ExpressionStatement).expression
+        value.should be_a BooleanLiteral
+        value.as(BooleanLiteral).value?.should be_false
+
+        expr.alternative.should be_nil
+      end
     end
   end
 
