9cb4b42e83fb8925fb885bdb5b553d7f5ce163cc
Myles La Verne Schultz <myleslschultz@icloud.com>
2023-06-10 03:33:26 -0400

    Updated main.swift so now it runs
    Calling swift run Lexer now calls the main function.

--- swift/Sources/Lexer/main.swift
+++ swift/Sources/Lexer/main.swift
@@ -1,10 +1,13 @@
 import ArgumentParser
 
-@main
 struct LexerCli: ParsableCommand {
    public static let configuration = CommandConfiguration(
        abstract: "A Swift command-line tool to lex a string."
    )
 
-    mutating func run() {}
+    mutating func run() {
+        print("main function needs to be implemented")
+    }
 }
+
+LexerCli.main()
