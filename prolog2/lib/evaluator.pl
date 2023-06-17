a9d49a136eb59b5df5774daa28cc121a03dcc50e
Luiz Fernando Afra Brito <luizfernandoafrabrito@gmail.com>
2023-06-17 16:35:29 -0300

    clean further

--- prolog2/lib/evaluator.pl
+++ prolog2/lib/evaluator.pl
@@ -17,9 +17,28 @@ evaluate(return(Exp), Env, Value) :-
 
 evaluate(if(Cond, Left, Right), Env, Value) :-
     evaluate(Cond, Env, CValue),
-    (\+ is_false(CValue) ->
+    CValue \= false, CValue \= nil ->
         evaluate(Left, Env, Value);
-        evaluate(Right, Env, Value)).
+        evaluate(Right, Env, Value).
+
+evaluate(op(Op, Left), Env, Value) :-
+    evaluate(Left, Env, LValue),
+    do_op(Op, LValue, Value).
+
+evaluate(op(ROp, RLeft, RRight), Env, Value) :-
+    reassociate(op(ROp, RLeft, RRight), op(LOp, LLeft, LRight)),
+    evaluate(LLeft, Env, LValue),
+    evaluate(LRight, Env, RValue),
+    do_op(LOp, LValue, RValue, Value).
+
+evaluate(index(L, Index), Env, Value) :-
+    evaluate(L, Env, LValue),
+    evaluate(Index, Env, IValue),
+    evaluate(index(LValue, IValue), Env, Value).
+
+evaluate(index([list|L], Index), _, Value) :- nth0(Index, L, Value).
+evaluate(index([hash|H], Key), _, Value) :- append(_, [Key, Value|_], H).
+evaluate(index(_, _), _, nil).
 
 evaluate(call(F, Args), Env, Value) :-
     evaluate(F, Env, FValue),
@@ -37,35 +56,13 @@ evaluate(call(rest, _), _, nil).
 evaluate(call(push, [[list|L], E]), _, L2) :- append([list|L], [E], L2).
 evaluate(call(push, [[hash|H], K, V]), _, H2) :- append([hash|H], [K, V], H2).
 evaluate(call(push, _), _, nil).
-evaluate(call(puts, L), _, nil) :- write_list(L).
+evaluate(call(puts, []), _, nil).
+evaluate(call(puts, [H|T]), _, nil) :- make_printable(H, Tmp), writeln(Tmp), evaluate(call(puts, T), _, nil).
 
 evaluate(call(function(Params, Body, FOuter), Args), _, Value) :-
     term_hash(function(Args, Body), FInner),
     asset_unique_all(FInner, Params, Args),
     evaluate(Body, env(FInner, FOuter), Value).
-
-evaluate(index(L, Index), Env, Value) :-
-    evaluate(L, Env, LValue),
-    evaluate(Index, Env, IValue),
-    evaluate(index(LValue, IValue), Env, Value).
-
-evaluate(index([list|L], Index), _, Value) :- nth0(Index, L, Value).
-evaluate(index([hash|H], Key), _, Value) :- append(_, [Key, Value|_], H).
-evaluate(index(_, _), _, nil).
-
-evaluate(op(not, Left), Env, Value) :-
-    evaluate(Left, Env, LValue),
-    do_not(LValue, Value).
-
-evaluate(op(neg, Left), Env, Value) :-
-    evaluate(Left, Env, LValue), 
-    do_neg(LValue, Value).
-
-evaluate(op(ROp, RLeft, RRight), Env, Value) :-
-    op_reassoc(op(ROp, RLeft, RRight), op(LOp, LLeft, LRight)),
-    evaluate(LLeft, Env, LValue),
-    evaluate(LRight, Env, RValue),
-    do_op(LOp, LValue, RValue, Value).
 
 evaluate(int(X), _, X).
 evaluate(bool(X), _, X).
@@ -92,6 +89,10 @@ evaluate_block_stms([_|Ss], Env, Value) :-  evaluate_block_stms(Ss, Env, Value).
 evaluate_expr_list([], _, []).
 evaluate_expr_list([A|As], Env, [V|Vs]) :- evaluate(A, Env, V), evaluate_expr_list(As, Env, Vs).
 
+make_printable([list|L], L) :- !.
+make_printable([hash|H], H) :- !.
+make_printable(X, X).
+
 assert_unique(Env, Key, Value) :-
     bindings(Env, Key, PValue),
     retract(bindings(Env, Key, PValue)),
@@ -105,18 +106,9 @@ asset_unique_all(Env, [ident(K)|Ks], [V|Vs]) :-
     assert_unique(Env, K, V),
     asset_unique_all(Env, Ks, Vs).
 
-write_list([]).
-write_list([H|T]) :- 
-    make_printable(H, Tmp), 
-    writenl(Tmp), nl, 
-    write_list(T).
 
-make_printable([list|L], L) :- !.
-make_printable([hash|H], H) :- !.
-make_printable(X, X).
-
-op_reassoc(op(Op1, X, op(Op2, Y, Z)), op(Op2, op(Op1, X, Y), Z)) :- op_prec(Op1, P), op_prec(Op2, P), !.
-op_reassoc(Op, Op).
+reassociate(op(Op1, X, op(Op2, Y, Z)), op(Op2, op(Op1, X, Y), Z)) :- op_prec(Op1, P), op_prec(Op2, P), !.
+reassociate(Op, Op).
 
 op_prec(eq, 0).
 op_prec(neq, 0).
@@ -127,29 +119,22 @@ op_prec(sub, 2).
 op_prec(mult, 3).
 op_prec(div, 3).
 
-do_not(A, true) :- is_false(A).
-do_not(A, false) :- \+ is_false(A).
-do_neg(A, NA) :- integer(A), NA is -A.
-do_neg(A, nil) :- \+ integer(A).
-do_op(eq, Left, Right, Value) :- is_eq(Left, Right, Value).
-do_op(neq, Left, Right, Value) :- is_neq(Left, Right, Value).
-do_op(lt, Left, Right, Value) :- is_lt(Left, Right, Value).
-do_op(gt, Left, Right, Value) :- is_gt(Left, Right, Value).
-do_op(add, Left, Right, Value) :- string(Left), string(Right), string_concat(Left, Right, Value).
-do_op(add, Left, Right, Value) :- number(Left), number(Right), Value is Left + Right.
-do_op(sub, Left, Right, Value) :- Value is Left - Right.
-do_op(div, Left, Right, Value) :- Value is Left / Right.
-do_op(mult, Left, Right, Value) :- Value is Left * Right.
+do_op(not, L, true) :- L = false; L = nil.
+do_op(not, L, false) :- L \= false, L \= nil.
+do_op(neg, L, V) :- number(L), V is -L.
+do_op(eq, L, R, true) :- L = R.
+do_op(eq, L, R, false) :- L \= R.
+do_op(neq, L, R, true) :- L \= R.
+do_op(neq, L, R, false) :- L = R.
+do_op(lt, L, R, true) :- numbers(L, R), L < R.
+do_op(lt, L, R, false) :- numbers(L, R), L >= R.
+do_op(gt, L, R, true) :- numbers(L, R), L > R.
+do_op(gt, L, R, false) :- numbers(L, R), L =< R.
+do_op(add, L, R, V) :- strings(L, R), string_concat(L, R, V).
+do_op(add, L, R, V) :- numbers(L, R), V is L + R.
+do_op(sub, L, R, V) :- numbers(L, R), V is L - R.
+do_op(div, L, R, V) :- numbers(L, R), V is L / R.
+do_op(mult, L, R, V) :- numbers(L, R), V is L * R.
 
-is_false(A) :- A = false; A = nil.
-is_eq(A, A, true) :- !.
-is_eq(_, _, false).
-is_neq(A, A, false) :- !.
-is_neq(_, _, true).
-is_lt(A, B, true) :- are_integers(A, B), A < B.
-is_lt(A, B, false) :- are_integers(A, B), A >= B.
-is_lt(A, B, nil) :- \+ are_integers(A, B).
-is_gt(A, B, true) :- are_integers(A, B), A > B.
-is_gt(A, B, false) :- are_integers(A, B), A =< B.
-is_gt(A, B, nil) :- \+ are_integers(A, B).
-are_integers(A, B) :- integer(A), integer(B).
+numbers(A, B) :- number(A), number(B).
+strings(A, B) :- string(A), string(B).
