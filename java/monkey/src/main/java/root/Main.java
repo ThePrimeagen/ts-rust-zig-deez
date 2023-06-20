46042451d54bf2c8b5ad58b5e6eb243a75935e20
Rafael Santana <rafaelns.br@gmail.com>
2023-06-20 20:09:19 -0300

    Fixing the Dockerfile, so it doesn't hang waiting for input we ran for checks

--- java/monkey/src/main/java/root/Main.java
+++ java/monkey/src/main/java/root/Main.java
@@ -1,14 +1,29 @@
 package root;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        String username = System.getProperty("user.name");
+        InputStream inputStream;
+        boolean printPrompt;
 
-        System.out.printf("Hello %s! This is the Monkey programming language!%n", username);
-        System.out.println("Feel free to type in commands!");
+        // Reading args as input if they exist
+        if (args.length > 0) {
+            inputStream = new ByteArrayInputStream(String.join(" ", args).getBytes());
+            printPrompt = false;
+        } else {
+            String username = System.getProperty("user.name");
 
-        REPL repl = new REPL(System.in, System.out);
+            System.out.printf("Hello %s! This is the Monkey programming language!%n", username);
+            System.out.println("Feel free to type in commands!");
+
+            inputStream = System.in;
+            printPrompt = true;
+        }
+
+        REPL repl = new REPL(inputStream, System.out, printPrompt);
         repl.start();
     }
 }
