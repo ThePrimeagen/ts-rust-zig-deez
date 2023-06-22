61fb66bdea249bc33c84c7014b98495c2b3e58cc
Vikas <v33vikasgautam@gmail.com>
2023-06-23 03:38:29 +0530

    Updated bb.edn and project.clj for importing vikasg7/jdsl

--- clj/project.clj
+++ clj/project.clj
@@ -4,4 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
+  :plugins [[lein-git-deps "0.0.2-SNAPSHOT"]]
+  :git-dependencies {"https://github.com/vikasg7/jdsl.git" "master"}
+  :global-vars {*warn-on-reflection* true}
   :repl-options {:init-ns clj.core})
