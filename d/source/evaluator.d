61b092821cfbaf5573eabe3ca6dc0d499711c54a
ToraKuma42 <95514041+ToraKuma42@users.noreply.github.com>
2023-06-27 08:17:55 -0500

    Added even more evaluation tests

--- d/source/evaluator.d
+++ d/source/evaluator.d
@@ -69,8 +69,9 @@ private Evaluator* prepareEvaluator(const string input)
 /// Basic integer test
 unittest
 {
-    const auto input = "5; 10;";
-    const long[2] expected = [5, 10];
+    const auto input = "5; 10;
+-5; -10";
+    const long[4] expected = [5, 10, -5, -10];
 
     auto evaluator = prepareEvaluator(input);
     evaluator.evalProgram();
@@ -80,6 +81,106 @@ unittest
         result.match!((long value) => assert(value == expected[i],
                 format("Number value %s does not match expected value %s", value, expected[i])),
                 _ => assert(false, format("Expected int value in statement %s", i)));
+    }
+}
+
+/// More complex integer test
+unittest
+{
+    const auto input = "5; 10; -5; -10;
+5 + 5 + 5 + 5 - 10;
+2 * 2 * 2 * 2 * 2;
+-50 + 100 + -50;
+5 * 2 + 10;
+5 + 2 * 10;
+20 + 2 * -10;
+50 / 2 * 2 + 10;
+2 * (5 + 10);
+3 * 3 * 3 + 10;
+3 * (3 * 3) + 10;
+(5 + 10 * 2 + 15 / 3) * 2 + -10;";
+
+    const long[15] expected = [5, 10, -5, -10, 10, 32, 0, 20, 25, 0, 60, 30, 37, 37, 50];
+
+    auto evaluator = prepareEvaluator(input);
+    evaluator.evalProgram();
+
+    foreach (i, result; evaluator.results[])
+    {
+        result.match!((long value) => assert(value == expected[i],
+                format("Number value %s does not match expected value %s", value, expected[i])),
+                _ => assert(false, format("Expected int value in statement %s", i)));
+    }
+}
+
+/// Basic boolean test
+unittest
+{
+    const auto input = "true; false;
+!true; !false;
+!5; !!true; !!false; !!5;
+";
+
+    const bool[8] expected = [true, false, false, true, false, true, false, true];
+
+    auto evaluator = prepareEvaluator(input);
+    evaluator.evalProgram();
+
+    foreach (i, result; evaluator.results[])
+    {
+        result.match!((bool value) => assert(value == expected[i],
+                format("Bool value %s does not match expected value %s", value, expected[i])),
+                _ => assert(false, format("Expected bool value in statement %s", i)));
+    }
+}
+
+/// Complex boolean test
+unittest
+{
+    const auto input = "true; false;
+1 < 2; 1 > 2;
+1 < 1; 1 > 1;
+1 == 1;
+1 != 1;
+1 == 2;
+1 != 2;
+";
+
+    const bool[10] expected = [true, false, true, false, false, false, true, false, false, true];
+
+    auto evaluator = prepareEvaluator(input);
+    evaluator.evalProgram();
+
+    foreach (i, result; evaluator.results[])
+    {
+        result.match!((bool value) => assert(value == expected[i],
+                format("Bool value %s does not match expected value %s", value, expected[i])),
+                _ => assert(false, format("Expected bool value in statement %s", i)));
+    }
+}
+
+/// Boolean evaluations test
+unittest
+{
+    const auto input = "true == true;
+false == false;
+true == false;
+true != false;
+false != true;
+(1 < 2) == true; (1 < 2) == false;
+(1 > 2) == true; (1 > 2) == false;
+";
+
+    const bool[9] expected = [true, true, false, true, true, true, false, false, true];
+
+    auto evaluator = prepareEvaluator(input);
+    evaluator.evalProgram();
+
+    foreach (i, result; evaluator.results[])
+    {
+        result.match!((bool value) => assert(value == expected[i],
+                format("Bool value %s does not match expected value %s", value, expected[i])),
+                _ => assert(false, format("Expected bool value in statement %s", i)));
     }
 }
 
@@ -94,7 +195,7 @@ unittest
 -(5 + 5);
 !(true == true);";
 
-    const int[7] expected = [false, true, 10, 20, 0, -10, false];
+    const long[7] expected = [false, true, 10, 20, 0, -10, false];
     auto evaluator = prepareEvaluator(input);
 
     evaluator.evalProgram();
